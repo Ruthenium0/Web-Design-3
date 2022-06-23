13d1f78d68e45bf856b089823f61ab32d537ba13
Ruthenium0 <54074649+Ruthenium0@users.noreply.github.com>
2022-06-23 15:59:29 +0300

    Add files via upload

--- back/db/db.js
+++ back/db/db.js
@@ -1,1 +1,21 @@
 
+
+const Sequelize = require("sequelize");
+
+const sequelize = new Sequelize({
+	dialect: "sqlite",
+	storage: "./db.sqlite3",
+});
+
+const Users = require("./model")(sequelize);
+
+// function createTable() {
+
+// }
+
+Users.sync()
+
+module.exports = {
+	sequelize: sequelize,
+	Users: Users,
+};
